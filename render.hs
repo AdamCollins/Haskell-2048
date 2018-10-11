1f2f822e7447110a3b463e30bb733575df13a92c
blindacai <linda_cai2014@hotmail.com>
2018-10-10 18:12:27 -0700

    generalize drawSquare function

--- render.hs
+++ render.hs
@@ -15,25 +15,17 @@ background = white
 window :: Display
 window = InWindow "Static 2048 Board" (round window_size, round window_size) (10, 10)
 
-drawSquare :: Picture
-drawSquare = translate (-1/2 * window_size + edge_size + 1/2 * square_size) (0) $ 
-             color (dark blue) $ rectangleSolid square_size square_size
-
-drawSquare2 :: Picture
-drawSquare2 = translate (-1/2 * window_size + edge_size * 2 + square_size + 1/2 * square_size) 0 $ 
-              color (dark yellow) $ rectangleSolid square_size square_size
-
-drawSquare3 :: Picture
-drawSquare3 = translate (1/2 * window_size - 2 * edge_size - square_size - 1/2 * square_size) 0 $ 
-              color (dark red) $ rectangleSolid square_size square_size
-
-drawSquare4 :: Picture
-drawSquare4 = translate (1/2 * window_size - edge_size - 1/2 * square_size) 0 $ 
-              color (dark green) $ rectangleSolid square_size square_size
+drawSquare :: Float -> Color -> Picture
+drawSquare index scolor = 
+  translate (-1/2 * window_size + edge_size * (index + 1) + square_size * index + 1/2 * square_size) 0 $ 
+  color scolor $ rectangleSolid square_size square_size
 
 drawing :: Picture
 drawing = translate (0) (1/2 * window_size - edge_size - 1/2 * square_size) $ 
-          pictures [drawSquare, drawSquare2, drawSquare3, drawSquare4]
+          pictures [drawSquare 0 (greyN 0.5), 
+                    drawSquare 1 (greyN 0.5), 
+                    drawSquare 2 (greyN 0.5), 
+                    drawSquare 3 (greyN 0.5)]
 
 main :: IO ()
 main = display window background drawing
