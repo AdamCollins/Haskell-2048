4612349df50b982f71483af5f1cf9e5a90ea0753
blindacai <linda_cai2014@hotmail.com>
2018-10-10 20:02:22 -0700

    add drawRow function

--- render.hs
+++ render.hs
@@ -10,7 +10,7 @@ edge_size = window_size / (numof_edges + squares_per_row * square_to_edge_ratio)
 square_size = edge_size * square_to_edge_ratio
 
 background :: Color
-background = white
+background = greyN 0.5
 
 window :: Display
 window = InWindow "Static 2048 Board" (round window_size, round window_size) (10, 10)
@@ -20,12 +20,16 @@ drawSquare index scolor =
   translate (-1/2 * window_size + edge_size * (index + 1) + square_size * index + 1/2 * square_size) 0 $ 
   color scolor $ rectangleSolid square_size square_size
 
+drawRow :: Float -> Picture
+drawRow index =
+  translate 0 (1/2 * window_size - edge_size * (index + 1) - square_size * index - 1/2 * square_size) $ 
+    pictures [drawSquare 0 (greyN 0.7), 
+              drawSquare 1 (greyN 0.7), 
+              drawSquare 2 (greyN 0.7), 
+              drawSquare 3 (greyN 0.7)]
+
 drawing :: Picture
-drawing = translate (0) (1/2 * window_size - edge_size - 1/2 * square_size) $ 
-          pictures [drawSquare 0 (greyN 0.5), 
-                    drawSquare 1 (greyN 0.5), 
-                    drawSquare 2 (greyN 0.5), 
-                    drawSquare 3 (greyN 0.5)]
+drawing = pictures [drawRow 0, drawRow 1, drawRow 2, drawRow 3]
 
 main :: IO ()
 main = display window background drawing
