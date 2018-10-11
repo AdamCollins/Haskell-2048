b9c3bcd3eb34bd27f21c6a9aa8d435bfceb0f6d3
blindacai <linda_cai2014@hotmail.com>
2018-10-10 17:58:40 -0700

    change hard coded row positions to values relative to the gui window

--- render.hs
+++ render.hs
@@ -1,30 +1,39 @@
 import Graphics.Gloss
 
-window_width, window_height :: Int
-window_width = 296 * 2
-window_height = window_width
+window_size :: Float
+window_size = 296 * 2
+squares_per_row = 4
+numof_edges = squares_per_row + 1
+square_to_edge_ratio = 8
+
+edge_size = window_size / (numof_edges + squares_per_row * square_to_edge_ratio)
+square_size = edge_size * square_to_edge_ratio
 
 background :: Color
 background = white
 
 window :: Display
-window = InWindow "Static 2048 Board" (window_width, window_height) (10, 10)
+window = InWindow "Static 2048 Board" (round window_size, round window_size) (10, 10)
 
 drawSquare :: Picture
-drawSquare = translate (-296 + 16 + 64) (0) $ color (dark blue) $ rectangleSolid 128 128
+drawSquare = translate (-1/2 * window_size + edge_size + 1/2 * square_size) (0) $ 
+             color (dark blue) $ rectangleSolid square_size square_size
 
 drawSquare2 :: Picture
-drawSquare2 = translate (-296 + 16 + 128 + 16 + 64) 0 $ color (dark yellow) $ rectangleSolid 128 128
+drawSquare2 = translate (-1/2 * window_size + edge_size * 2 + square_size + 1/2 * square_size) 0 $ 
+              color (dark yellow) $ rectangleSolid square_size square_size
 
 drawSquare3 :: Picture
-drawSquare3 = translate (296 - 16 - 128 - 16 - 64) 0 $ color (dark red) $ rectangleSolid 128 128
+drawSquare3 = translate (1/2 * window_size - 2 * edge_size - square_size - 1/2 * square_size) 0 $ 
+              color (dark red) $ rectangleSolid square_size square_size
 
 drawSquare4 :: Picture
-drawSquare4 = translate (296 - 16 - 64) 0 $ color (dark green) $ rectangleSolid 128 128
+drawSquare4 = translate (1/2 * window_size - edge_size - 1/2 * square_size) 0 $ 
+              color (dark green) $ rectangleSolid square_size square_size
 
 drawing :: Picture
-drawing = pictures
-  [drawSquare, drawSquare2, drawSquare3, drawSquare4]
+drawing = translate (0) (1/2 * window_size - edge_size - 1/2 * square_size) $ 
+          pictures [drawSquare, drawSquare2, drawSquare3, drawSquare4]
 
 main :: IO ()
 main = display window background drawing
