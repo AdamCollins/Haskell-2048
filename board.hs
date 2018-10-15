586c7ff94a3131700dcc1929a86d653971052300
Adam Collins <adamcollinssecure@gmail.com>
2018-10-15 08:31:13 -0700

    Added isRowEmpty

--- board.hs
+++ board.hs
@@ -1,5 +1,6 @@
-module Board (initBoard, insert) where
+module Board (initBoard, insertTile, rotate) where
     import System.IO
+    import Data.List
     type Tile = Int
     type Row = [Tile]
     type Board = [[Tile]]
@@ -10,15 +11,26 @@ module Board (initBoard, insert) where
                 [0, 0, 0]]
 
      -- Inserts a new tile into the board at an 0 location
-    insert :: Board -> Board
-    insert [[]] = [[]]
-    insert (r1:b) = (\(row,inserted) -> if inserted then (row:b) else (row : (insert b))) $ insertTileToRow r1
+    insertTile :: Board -> Board
+    insertTile [[]] = [[]]
+    insertTile (r1:b) = (\(row,inserted) -> if inserted then (row:b) else (row : (insertTile b))) $ insertTileToRow r1
 
     -- Helper for insert
     insertTileToRow :: Row -> (Row, Bool)
     insertTileToRow [] = ([], False)
     insertTileToRow (h:t) = if(h==0) then ((2:t), True) else (\ (lst,bool) -> (h:lst,bool)) $ insertTileToRow t 
     
+    rotate :: [Char] -> Board -> Board  
+    rotate dir board 
+                    | dir=="left" = reverse $ transpose board
+                    | dir== "right" = transpose $ reverse board
+                    | dir=="up" = rotate "left" (rotate "left" board)
+                    | otherwise = board
+    
+    isRowEmpty :: Row -> Bool 
+    isRowEmpty (h:t) = foldr (\ h b -> h==0&&b) True (h:t) 
+
+
     --Prints Board in the console
     printBoard :: Show a => [a] -> IO ()
     printBoard b = mapM_ putStrLn $ (\ lst -> [show row | row<-lst]) b
